7f783edad6c93630de3261f6dd99b5c6372b57de
Reiko ofc <reikoofc@gmail.com>
2022-12-26 22:31:28 -0300

    WhatsApp bot🔰

---
+++ configuracoes.js
@@ -0,0 +1,13 @@
+/*Mude apenas oq tá dentro das 'aspas' ou mude o diretório.*/
+
+const fs = require('fs')
+
+foto = fs.readFileSync('./lib/bot.jpg') //foto do menu, altere a foto na pasta lib
+
+nomeseção = 'web' //nome do arquivo de conexão
+
+fazendo = '*_Carregando comando\nAguarde alguns segundos_*' //mensagem de carregamento
+
+pacote = 'NABUTO'
+
+autor = 'bot'
